280c026bbe769d63f416671f72dfe7b5a59d9d90
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-03 20:38:51 -0300

    test: ensure DbCreateAccount call CreateAccountRepository if correct values

---
+++ tests/data/usecases/DbCreateAccount.spec.ts
@@ -0,0 +1,30 @@
+import { DbCreateAccount } from '@/data/usecases/account/createAccount/DbCreateAccount';
+
+import { ICreateAccountRepository } from '@/data/protocols/db/account/ICreateAccountRepository';
+
+import { mockCreateAccountRepository } from '@/tests/data/mocks';
+import { mockAccountDTO } from '@/tests/domain/mocks';
+
+type ISutTypes = {
+  sut: DbCreateAccount
+  createAccountRepositoryStub: ICreateAccountRepository
+}
+
+const makeSut = (): ISutTypes => {
+  const createAccountRepositoryStub = mockCreateAccountRepository();
+  const sut = new DbCreateAccount(createAccountRepositoryStub);
+
+  return {
+    sut,
+    createAccountRepositoryStub,
+  };
+};
+
+describe('DbCreateAccount Usecase', () => {
+  it('Should call CreateAccountRepository with correct values', () => {
+    const { sut, createAccountRepositoryStub } = makeSut();
+    const createSpy = jest.spyOn(createAccountRepositoryStub, 'create');
+    sut.create(mockAccountDTO());
+    expect(createSpy).toHaveBeenCalledWith(mockAccountDTO());
+  });
+});
