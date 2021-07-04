93135e8ea68ebd5b90a6abfe9ce88ccdea1316cd
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-04 18:03:54 -0300

    feat: ensure calls Validation with correct values

--- src/presentation/controllers/login/signUp/SignUpController.ts
+++ src/presentation/controllers/login/signUp/SignUpController.ts
@@ -1,12 +1,16 @@
 import {
-  IController, ICreateAccount, IHttpRequest, IHttpResponse, serverError, success,
+  IController, ICreateAccount, IHttpRequest, IHttpResponse, IValidation, serverError, success,
 } from '@/presentation/controllers/login/signUp/SignUpControllerProtocols';
 
 export class SignUpController implements IController {
-  constructor(private readonly createAccount: ICreateAccount) {}
+  constructor(
+    private readonly createAccount: ICreateAccount,
+    private readonly validation: IValidation,
+  ) {}
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
+      this.validation.validate(httpRequest.body);
       const { name, email, password } = httpRequest.body;
       const account = await this.createAccount.create({ name, email, password });
       if (account) {
