3f6807c13efdc384c209937f06f02a179f3285c1
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-06 20:54:32 -0300

    test: ensure CompareFieldsValidation not return if validation succeeds

--- tests/validation/validators/CompareFieldsValidation.spec.ts
+++ tests/validation/validators/CompareFieldsValidation.spec.ts
@@ -14,4 +14,13 @@ describe('Compare Fields Validation', () => {
     });
     expect(error).toEqual(new InvalidParamError('fieldToCompare'));
   });
+
+  it('should not return if validation succeeds', () => {
+    const sut = makeSut();
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value',
+    });
+    expect(error).toBeFalsy();
+  });
 });
