055324a152cede3c71e9995fabd327693b51160e
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-05 22:49:16 -0300

    test: ensure ValidationComposite return first error

--- tests/validation/validators/ValidationComposite.spec.ts
+++ tests/validation/validators/ValidationComposite.spec.ts
@@ -26,7 +26,15 @@ describe('Validation Composite', () => {
   it('should return error if any validation fails', () => {
     const { sut, validationStub } = makeSut();
     jest.spyOn(validationStub[0], 'validate').mockReturnValueOnce(new MissingParamError('field'));
-    const error = sut.validate({ field: 'any_value' });
+    const error = sut.validate({ wrong_field: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
+  });
+
+  it('should return the first error if more then one validation fails', () => {
+    const { sut, validationStub } = makeSut();
+    jest.spyOn(validationStub[0], 'validate').mockReturnValueOnce(new Error());
+    jest.spyOn(validationStub[1], 'validate').mockReturnValueOnce(new MissingParamError('field'));
+    const error = sut.validate({ wrong_field: 'any_field' });
+    expect(error).toEqual(new Error());
   });
 });
