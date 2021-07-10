9eeae724e7b758631ef9044413ecb395beecf9f1
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-10 12:00:57 -0300

    feat: ensure EmailValidator return false if validator returns false

---
+++ src/infra/validators/EmailValidatorAdapter.ts
@@ -0,0 +1,9 @@
+import validator from 'validator';
+
+import { IEmailValidator } from '@/validation/protocols/IEmailValidator';
+
+export class EmailValidatorAdapter implements IEmailValidator {
+  isValid(email: string): boolean {
+    return validator.isEmail(email);
+  }
+}
