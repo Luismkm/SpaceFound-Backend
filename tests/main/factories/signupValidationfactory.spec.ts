d4732afca8b75838fb98b417f8d82807910018fa
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-10 11:33:02 -0300

    test: ensure ValidationComposite be called with all validations

---
+++ tests/main/factories/signupValidationfactory.spec.ts
@@ -0,0 +1,24 @@
+import {
+  CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite,
+} from '@/validation/validators';
+import { makeSignUpValidation } from '@/main/factories/controllers/login/signUp/signUpValidationfactory';
+
+import { IValidation } from '@/presentation/protocols';
+
+import { mockEmailValidator } from '@/tests/validation/mocks/mockEmailValidator';
+
+jest.mock('@/validation/validators/ValidationComposite');
+
+describe('SignUp Validation Factory', () => {
+  it('Should call ValidationComposite with all validations', () => {
+    makeSignUpValidation();
+    const validations: IValidation[] = [];
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field));
+    }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'));
+    validations.push(new EmailValidation('email', mockEmailValidator()));
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations);
+  });
+});
