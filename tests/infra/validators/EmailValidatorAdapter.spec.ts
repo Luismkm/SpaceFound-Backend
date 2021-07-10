7486a422fbc1eedfa0b676be24954dab55355836
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-10 12:01:12 -0300

    test: ensure EmailValidator return false if validator returns false

---
+++ tests/infra/validators/EmailValidatorAdapter.spec.ts
@@ -0,0 +1,23 @@
+import validator from 'validator';
+
+import { EmailValidatorAdapter } from '@/infra/validators/EmailValidatorAdapter';
+
+jest.mock('validator', () => ({
+  isEmail(email: string): boolean {
+    return true;
+  },
+}));
+
+let sut: EmailValidatorAdapter;
+
+beforeEach(() => {
+  sut = new EmailValidatorAdapter();
+});
+
+describe('EmailValidator Adapter', () => {
+  it('should return false if validator return false', () => {
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
+    const isValid = sut.isValid('invalid_email');
+    expect(isValid).toBeFalsy();
+  });
+});
