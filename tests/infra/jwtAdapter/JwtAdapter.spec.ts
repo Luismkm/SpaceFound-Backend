f5b7ba029b1144e64801ee78f4475e317c4ab529
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-13 20:43:46 -0300

    test: ensure JWT Adapter sign return a token

--- tests/infra/jwtAdapter/JwtAdapter.spec.ts
+++ tests/infra/jwtAdapter/JwtAdapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from '@/infra/cryptography/jwtAdapter/JwtAdapter';
 
+jest.mock('jsonwebtoken', () => ({
+  sign(): string {
+    return 'any_token';
+  },
+}));
+
 const makeSut = (): JwtAdapter => new JwtAdapter('secret');
 
 describe('JWT Adapter', () => {
@@ -9,5 +15,11 @@ describe('JWT Adapter', () => {
     const signSpy = jest.spyOn(jwt, 'sign');
     sut.encrypt('any_id');
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
+  });
+
+  it('should return a token sign success', () => {
+    const sut = makeSut();
+    const accessToken = sut.encrypt('any_id');
+    expect(accessToken).toBe('any_token');
   });
 });
