00b6627d9a0bd186be367080e3cf6241abd9b26f
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-13 20:18:00 -0300

    feat: ensure JWT Adapter call sign with correct values

---
+++ src/infra/cryptography/jwtAdapter/JwtAdapter.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+
+import { IEncrypter } from '@/data/protocols/cryptography/IEncrypter';
+
+export class JwtAdapter implements IEncrypter {
+  constructor(private readonly secret: string) {}
+
+  encrypt(value: string): string {
+    const accessToken = jwt.sign({ id: value }, this.secret);
+    return accessToken;
+  }
+}
