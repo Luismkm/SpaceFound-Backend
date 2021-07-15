7ffb67f8cc3d6770445596f3599239693781e5dc
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-14 22:27:13 -0300

    test: ensure DbAuthentication call HashComparer with correct values

--- tests/data/usecases/DbAuthentication.spec.ts
+++ tests/data/usecases/DbAuthentication.spec.ts
@@ -1,23 +1,29 @@
-import { mockAuthentication, throwError } from '@/tests/domain/mocks';
 import { DbAuthentication } from '@/data/usecases/account/authentication/DbAuthentication';
-import { ILoadAccountByEmailRepository } from '@/data/protocols/db/account/ILoadAccountByEmailRepository';
 
-import { mockLoadAccountByEmailRepository } from '../mocks';
+import { ILoadAccountByEmailRepository } from '@/data/protocols/db/account/ILoadAccountByEmailRepository';
+import { IHashComparer } from '@/data/protocols/cryptography/IHashComparer';
+
+import { mockAuthentication, throwError } from '@/tests/domain/mocks';
+import { mockHashComparer, mockLoadAccountByEmailRepository } from '../mocks';
 
 type ISutTypes = {
   sut: DbAuthentication
   loadAccountByEmailRepositoryStub: ILoadAccountByEmailRepository
+  hashCompareStub: IHashComparer
 }
 
 const makeSut = (): ISutTypes => {
   const loadAccountByEmailRepositoryStub = mockLoadAccountByEmailRepository();
+  const hashCompareStub = mockHashComparer();
   const sut = new DbAuthentication(
     loadAccountByEmailRepositoryStub,
+    hashCompareStub,
   );
 
   return {
     sut,
     loadAccountByEmailRepositoryStub,
+    hashCompareStub,
   };
 };
 
@@ -42,5 +48,12 @@ describe('DbAuthentication UseCase', () => {
     jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(null);
     const account = await sut.auth(mockAuthentication());
     expect(account).toBeNull();
+  });
+
+  it('Should call HashComparer with correct values', async () => {
+    const { sut, hashCompareStub } = makeSut();
+    const compareSpy = jest.spyOn(hashCompareStub, 'compare');
+    await sut.auth(mockAuthentication());
+    expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password');
   });
 });
