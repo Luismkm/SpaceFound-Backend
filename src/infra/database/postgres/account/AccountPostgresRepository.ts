f3118863d73e4d7d7cab3337c028e4d6ea2367f4
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-15 22:22:50 -0300

    feat: ensure AccountPostgresRepository return an account on loadByEmail

--- src/infra/database/postgres/account/AccountPostgresRepository.ts
+++ src/infra/database/postgres/account/AccountPostgresRepository.ts
@@ -3,12 +3,21 @@ import { KnexHelper } from '@/main/helpers/index2';
 import { ICreateAccountRepository } from '@/data/protocols';
 import { IAccount } from '@/domain/models/IAccount';
 import { ICreateAccountDTO } from '@/domain/usecases/account/ICreateAccount';
+import { ILoadAccountByEmailRepository } from '@/data/protocols/db/account/ILoadAccountByEmailRepository';
 
-export class AccountPostgresRepository implements ICreateAccountRepository {
+export class AccountPostgresRepository implements
+ ICreateAccountRepository,
+ ILoadAccountByEmailRepository {
   async create(account: ICreateAccountDTO): Promise<IAccount> {
     const { name, email, password } = account;
     const insertedUserId = await KnexHelper.knex('users').insert({ name, email, password }).returning('id');
     const userId = insertedUserId[0];
     return { ...account, id: userId };
+  }
+
+  async loadByEmail(value: string): Promise<any> {
+    const email = value;
+    const account = await KnexHelper.knex('users').where('email', email);
+    return account[0];
   }
 }
