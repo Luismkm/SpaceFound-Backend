366b98371ecb7169f4a6abe72d326843590269dd
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-17 20:55:40 -0300

    refactor: remove unused code

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,18 +1,6 @@
-import express from 'express';
 import { knexHelper } from '../infra/database/helpers/index';
-/* import cors from 'cors';
-import path from 'path'; */
-import routes from './routes';
 
-const app = express();
-app.use(routes);
-
-/* app.use(cors());
-app.use(express.json()); */
-/* KnexHelper.connect('production');
-
-app.listen(3333, () => console.log('Server running at port 3333')); */
-
-knexHelper.connect('production').then(() => {
+knexHelper.connect('production').then(async () => {
+  const app = (await import('./config/app')).default;
   app.listen(3333, () => console.log('Server running at port 3333'));
 }).catch(console.error);
