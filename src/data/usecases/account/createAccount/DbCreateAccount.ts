c7c62d045e86945c7abb39ea1af3623b3187934d
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-17 09:32:11 -0300

    feat: ensure DbCreateAccount return null if user exists

--- src/data/usecases/account/createAccount/DbCreateAccount.ts
+++ src/data/usecases/account/createAccount/DbCreateAccount.ts
@@ -1,4 +1,5 @@
 import { ICreateAccountRepository } from '@/data/protocols/db/account/ICreateAccountRepository';
+import { ILoadAccountByEmailRepository } from '@/data/protocols/db/account/ILoadAccountByEmailRepository';
 import {
   IAccount, ICreateAccount, ICreateAccountDTO, IHasher,
 } from './DbCreateAccountProtocols';
@@ -7,13 +8,18 @@ export class DbCreateAccount implements ICreateAccount {
   constructor(
     private readonly hasher: IHasher,
     private readonly createAccountRepository: ICreateAccountRepository,
+    private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
   ) {}
 
   async create(account: ICreateAccountDTO): Promise<IAccount> {
-    const passwordHashed = await this.hasher.hash(account.password);
-    const accountCreated = await this.createAccountRepository.create({
-      ...account, password: passwordHashed,
-    });
-    return accountCreated;
+    const checkUserExists = await this.loadAccountByEmailRepository.loadByEmail(account.email);
+    if (!checkUserExists) {
+      const passwordHashed = await this.hasher.hash(account.password);
+      const accountCreated = await this.createAccountRepository.create({
+        ...account, password: passwordHashed,
+      });
+      return accountCreated;
+    }
+    return null;
   }
 }
