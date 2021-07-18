209c77bca67a3cfd1fe71bf3e19ddd5303d062a0
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-18 20:19:35 -0300

    feat: ensure LoginValidation  call ValidationComposite with all validations

---
+++ src/main/factories/controllers/login/login/loginValidationFactory.ts
@@ -0,0 +1,12 @@
+import { EmailValidatorAdapter } from '@/infra/validators/EmailValidatorAdapter';
+import { IValidation } from '@/presentation/protocols';
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '@/validation/validators';
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: IValidation[] = [];
+  for (const field of ['email', 'password']) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
+  return new ValidationComposite(validations);
+};
