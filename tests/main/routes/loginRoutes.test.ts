122ce6e33bd3c880632f64566bb3eb2c0de995b8
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-20 19:42:53 -0300

    test: ensure login route return 200 on success

--- tests/main/routes/loginRoutes.test.ts
+++ tests/main/routes/loginRoutes.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import { hash } from 'bcrypt';
 
 import app from '@/main/config/app';
 import { knexHelper } from '@/infra/database/helpers';
@@ -8,7 +9,7 @@ describe('Login Routes', () => {
     knexHelper.connect('development');
   });
 
-  beforeEach(async () => {
+  afterEach(async () => {
     await knexHelper.knex('users').delete('*');
   });
   describe('POST /signup', () => {
@@ -20,6 +21,24 @@ describe('Login Routes', () => {
           email: 'any_email@email.com',
           password: 'any_password',
           passwordConfirmation: 'any_password',
+        })
+        .expect(200);
+    });
+  });
+
+  describe('POST /login', () => {
+    it('should return 200 on login', async () => {
+      const password = await hash('123', 12);
+      await knexHelper.knex('users').insert({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password,
+      });
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@email.com',
+          password: '123',
         })
         .expect(200);
     });
