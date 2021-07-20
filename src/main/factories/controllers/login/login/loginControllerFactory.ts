f46c223fec39f500c02f192c394cb8f6a7565122
Luismkm <luismarcelo13@yahoo.com.br>
2021-07-20 19:42:42 -0300

    feat: ensure login route return 200 on success

---
+++ src/main/factories/controllers/login/login/loginControllerFactory.ts
@@ -0,0 +1,9 @@
+import { makeDbAuthentication } from '@/main/factories/usecases/account/authentication/DbAuthenticationFactory';
+import { LoginController } from '@/presentation/controllers/login/login/LoginController';
+import { IController } from '@/presentation/protocols';
+import { makeLoginValidation } from './loginValidationFactory';
+
+export const makeLoginController = (): IController => {
+  const controller = new LoginController(makeDbAuthentication(), makeLoginValidation());
+  return controller;
+};
--- src/main/routes/loginRoutes.ts
+++ src/main/routes/loginRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapter/express/expressRouterAdapter';
+import { makeLoginController } from '../factories/controllers/login/login/loginControllerFactory';
 import { makeSignUpController } from '../factories/controllers/login/signUp/signUpControllerFactory';
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()));
+  router.post('/login', adaptRoute(makeLoginController()));
 };
