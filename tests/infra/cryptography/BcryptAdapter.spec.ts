90a06c826d58cde95df9e01fc37b0a52b97fc1c5
Luismkm <luismarcelo13@yahoo.com.br>
2023-02-11 15:53:13 -0300

    test: add compare hash tests

--- tests/infra/cryptography/BcryptAdapter.spec.ts
+++ tests/infra/cryptography/BcryptAdapter.spec.ts
@@ -7,6 +7,10 @@ jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
     return Promise.resolve('hash');
   },
+
+  async compare(): Promise<boolean> {
+    return Promise.resolve(true);
+  },
 }));
 
 const salt = 12;
@@ -34,4 +38,26 @@ describe('Bcrypt Adapter', () => {
       await expect(promise).rejects.toThrow();
     });
   });
+
+  describe('hash()', () => {
+    it('should call compare with correct value', async () => {
+      const sut = makeSut();
+      const hashSpy = jest.spyOn(bcrypt, 'compare');
+      await sut.compare('any_value', 'any_hash');
+      expect(hashSpy).toHaveBeenCalledWith('any_value', 'any_hash');
+    });
+
+    it('should return true when compare succeeds', async () => {
+      const sut = makeSut();
+      const isValid = await sut.compare('any_value', 'any_hash');
+      expect(isValid).toBe(true);
+    });
+
+    it('should throw if compare throws', async () => {
+      const sut = makeSut();
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => { throw new Error(); });
+      const promise = sut.compare('any_value', 'any_hash');
+      await expect(promise).rejects.toThrow();
+    });
+  })
 });
