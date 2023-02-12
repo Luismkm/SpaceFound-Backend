d63084e243748ebd804765199f003ce3bd3f21c7
Luismkm <luismarcelo13@yahoo.com.br>
2023-02-12 13:22:06 -0300

    test: add integration test to loadByEmail

--- tests/infra/database/postgres/user/UserPostgresRepository.spec.ts
+++ tests/infra/database/postgres/user/UserPostgresRepository.spec.ts
@@ -36,11 +36,29 @@ describe('Account Postgres Repository', () => {
     });
   });
 
-  /* describe('findById', () => {
+  describe('loadByEmail', () => {
+    it('should return an account on loadByEmail success', async () => {
+      await sut.create(mockAccount());
+      const account = await sut.loadByEmail('any_email');
+      expect(account.name).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe('any_name');
+      expect(account.email).toBe('any_email');
+      expect(account.password).toBe('any_password');
+      await knexHelper.knex('user').delete('*');
+    });
+
+    it('should return null if loadByEmail fails', async () => {
+      const account = await sut.loadByEmail('any_email');
+      expect(account).toBeFalsy();
+    });
+  });
+
+  describe('findById', () => {
     it('should return true on update with success', async () => {
       const succeeds = await sut.update(mockParams());
       expect(succeeds).toBeTruthy();
       await knexHelper.knex('user').delete('*');
     });
-  }); */
+  });
 });
