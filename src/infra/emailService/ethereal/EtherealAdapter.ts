c343050769a98d30d3d006d7beb5c8b3406b38a4
Luismkm <luismarcelo13@yahoo.com.br>
2023-02-12 13:20:35 -0300

    feat: add singleton to connect ethereal email

--- src/infra/emailService/ethereal/EtherealAdapter.ts
+++ src/infra/emailService/ethereal/EtherealAdapter.ts
@@ -1,31 +1,20 @@
-import nodemailer, { Transporter } from 'nodemailer';
-
+import nodemailer from 'nodemailer';
 import { IMailTemplateProvider } from '@/infra/emailTemplate/HandlebarsMailTemplateProvider';
-import ISendEmailService from '@/data/protocols/emailService/ISendEmailService';
 import ISendMailDTO from '@/data/protocols/emailService/ISendEmailDTO';
+import { ISendEmailService } from '@/data/protocols/emailService/ISendEmailService';
+import { EtherealSingleton } from './EtherealSingleton';
 
 export class EtherealAdapter implements ISendEmailService {
-  private client: Transporter;
-
   constructor(
     private readonly template: string,
     private readonly templateProvider: IMailTemplateProvider,
-  ) {
-    nodemailer.createTestAccount().then((account) => {
-      const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: account.smtp.secure,
-        auth: {
-          user: 'elliot.auer85@ethereal.email',
-          pass: 'T6kw72XNcUvhhC2DdF',
-        },
-      });
-      this.client = transporter;
-    });
-  }
+  ) {}
+
   public async send({ to, from, subject, userData }: ISendMailDTO): Promise<void> {
-    const message = await this.client.sendMail({
+    const singletonInstance = EtherealSingleton.getInstance();
+    const transporter = singletonInstance.getTransporter();
+
+    const message = await transporter.sendMail({
       from: {
         name: 'Distribuidora',
         address: 'testesmtpgomail@gmail.com',
