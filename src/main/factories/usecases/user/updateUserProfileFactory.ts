b1b6a3174f40fe86c82d1932ebf5cd9613f97fb5
Luismkm <luismarcelo13@yahoo.com.br>
2023-03-05 16:52:10 -0300

    refactor: update user profile route

--- src/main/factories/usecases/user/updateUserProfileFactory.ts
+++ src/main/factories/usecases/user/updateUserProfileFactory.ts
@@ -1,13 +1,11 @@
 import { UserPostgresRepository } from '@/infra/database/postgres/user/UserPostgresRepository';
 import { IUpdateUserProfile } from '@/domain/usecases/user/IUpdateUserProfile';
-import { AccountPostgresRepository } from '@/infra/database/postgres/user/AccountPostgresRepository';
 import { DbUpdateUserProfile } from '@/data/usecases/user/DbUpdateUserProfile';
 
 export const makeDbUpdateUserProfile = (): IUpdateUserProfile => {
   const userPostgresRepository = new UserPostgresRepository();
-  const accountPostgresRepository = new AccountPostgresRepository();
   return new DbUpdateUserProfile(
-    accountPostgresRepository,
+    userPostgresRepository,
     userPostgresRepository,
   );
 };
--- src/main/routes/userRoutes.ts
+++ src/main/routes/userRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { adaptRoute } from '@/main/adapter/express/expressRouterAdapter';
 import { makeSignUpController } from '@/main/factories/controllers/user/signUpControllerFactory';
-import { makeLoginController } from '../factories/controllers/user/loginControllerFactory';
+import { makeLoginController } from '@/main/factories/controllers/user/loginControllerFactory';
+import { makeUpdateProfileController } from '@/main/factories/controllers/user/updateUserProfileControllerFactory';
+import { auth } from '@/main/middlewares/auth';
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()));
   router.post('/login', adaptRoute(makeLoginController()));
+  router.put('/user/profile', auth, adaptRoute(makeUpdateProfileController()));
 };
